3427eadc7c5b9fcdb4f9eb0e3a339b1758621690
Mark F. Brown <mark.brown314@gmail.com>
2020-12-09 20:48:50 -0500

    Changing approach
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-7/solution.py
+++ day-7/solution.py
@@ -4,38 +4,24 @@
 import sys
 import re
 
-def depth(luggage_dict, key):
-    tree = [key]
-    print("key:", key)
-    count = 0
-    while tree:
-        item = tree.pop()
-        print("item", item)
-        count += 1
-        if item not in luggage_dict:
-            continue
-        tree += luggage_dict[item]
-    return count
-
 def search(luggage_dict, key, find):
     tree = [key]
     print("key:", key)
-    count = 0
     while tree:
         item = tree.pop()
-        print("item", item)
+        print("item:", item)
         if item == find:
             print("found!")
             return True
         if item not in luggage_dict:
             continue
-        tree += luggage_dict[item]
+        tree += [bag[1] for bag in [contents for contents in luggage_dict[item]]]
     return False
 
 def main():
     filename = "puzzle_test.txt" if len(sys.argv) < 2 else sys.argv[1]
     rules = open(filename).read().splitlines()
-    rules = [re.sub("bags|bag|\.| |[0-9]|contain no other ", "", rule) for rule in rules]
+    rules = [re.sub(r"bags|bag|\.| |contain no other ", "", rule) for rule in rules]
 
     luggage_dict = dict()
 
@@ -43,11 +29,12 @@ def main():
         relations = rule.split('contain')
         print("*key:", relations[0])
         if len(relations) > 1:
-            members  = relations[1].split(',')
-            print("*members:", members)
-            luggage_dict[relations[0]] = members
-    #for key in luggage_dict:
-    print(sum([(search(luggage_dict, key,"shinygold")) for key in luggage_dict]) - 1)
+            members = relations[1].replace(',', ' ')
+            a = re.findall(r"(\d+)(\w+)", members)[0]
+            luggage_dict[relations[0]] = [(a[0], a[1], False)]
+            print("*members:", members, luggage_dict[relations[0]])
+
+    print(sum([(search(luggage_dict, key, "shinygold")) for key in luggage_dict])-1)
 
 if __name__ == "__main__":
     main()
