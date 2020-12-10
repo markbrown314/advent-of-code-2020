a04a0362d6a97be20cd695a917a0a8402126bb89
Mark F. Brown <mark.brown314@gmail.com>
2020-12-10 18:41:32 -0500

    Solution to Day 8 Part 1 & 2
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-8/solution.py
+++ day-8/solution.py
@@ -1,27 +1,59 @@
 #!/usr/bin/python3
-
+"""
+🎅 Advent of Code Day 8 Handheld Halting
+Mark F. Brown <mark.brown314@gmail.com>
+"""
 import sys
+import copy
 
-def nop(pos, acc, arg):
-    pass
-def acc(pos, acc, arg):
-    return acc + arg
-def jmp(pos, acc, arg):
-    return pos + arg
 def list_program(program):
     line_no = 0
     for line in program:
         print(line_no, ": ", line[0], line[1], sep="")
         line_no += 1
 
+def toggle_jmp_nop(program, line_no):
+    if program[line_no][0] == "nop":
+        program[line_no][0] = "jmp"
+    else:
+        program[line_no][0] = "nop"
+
+
+def debug_program(program):
+    change_points = []
+    line_no = 0
+    for line in program:
+        if line[0] == "nop" or line[0] == "jmp":
+            change_points.append(line_no)
+        line_no += 1
+
+    print(change_points)
+
+    while True:
+        test_program = copy.deepcopy(program)
+
+        if not change_points:
+            print("exhausted change points")
+            return
+
+        change_point = change_points.pop()
+        toggle_jmp_nop(test_program, change_point)
+        list_program(test_program)
+        if exec_program(test_program):
+            return
+
 def exec_program(program):
     acc = 0
     line_no = 0
+    lines = len(program)
     while True:
-        print(program[line_no])
+        if line_no >= lines:
+            print("reached end of program acc", acc)
+            return True
+
         if program[line_no][2]:
-            print("repeated line at:", line_no, "acc", acc)
-            break
+            print("loop detected at:", line_no, "acc", acc)
+            return False
         if program[line_no][0] == "nop":
             program[line_no][2] = True 
             line_no += 1
@@ -35,14 +67,21 @@ def exec_program(program):
             acc += int(program[line_no][1])
             line_no += 1
             continue
-
+    return False
 def main():
     filename = "puzzle_test.txt" if len(sys.argv) < 2 else sys.argv[1]
     file_input = open(filename).read().splitlines()
     program = [data.split() + [False] for data in file_input]
 
-    #list_program(program)
-    exec_program(program)
-    
+    print ("first run:")
+
+    list_program(program)
+    test_program = copy.deepcopy(program)
+    exec_program(test_program)
+
+    print ("debug run:")
+
+    debug_program(program)
+
 if __name__ == "__main__":
     main()
