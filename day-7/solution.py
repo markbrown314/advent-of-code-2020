07a98ea986be232772d81a40da72759ba7775243
Mark F. Brown <mark.brown314@gmail.com>
2020-12-10 10:22:59 -0500

    Fixed Day 7 Part 1 that broke in last commit
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-7/solution.py
+++ day-7/solution.py
@@ -5,50 +5,55 @@ import sys
 import re
 from treelib import Tree, Node
 
+def search_luggage(luggage_dict, key, find):
+    stack = [(1, key)]
+    while stack:
+        item = stack.pop()
+        if item[1] == find:
+            return True
+        if item[1] not in luggage_dict:
+            continue
+        stack += luggage_dict[item[1]]
+    return False
+
 def main():
     filename = "puzzle_test.txt" if len(sys.argv) < 2 else sys.argv[1]
     rules = open(filename).read().splitlines()
+ 
+    # parse input file into a dictionary
+    # (bag count, bag name)
+
     rules = [re.sub(r"bags|bag|\.| |contain no other ", "", rule) for rule in rules]
 
     luggage_dict = dict()
-    tree_dict = dict()
  
     for rule in rules:
         relations = rule.split('contain')
-        print("*key:", relations[0])
         if len(relations) > 1:
             members = relations[1].replace(',', ' ')
             data = re.findall(r"(\d+)(\w+)", members)
-            print("data:", data)
             luggage_dict[relations[0]] = data
-    print(luggage_dict)
+
+    # generate a dictionary of trees mapping each luggage relationship
+    # (bag count, bag name)
+
+    tree_dict = dict()
+
     for data in luggage_dict:
         tree_dict[data] = Tree()
-        root_node = Node(tag = data, data = data)
+        root_node = Node(tag = ('1', data), data = data)
         parent_id = root_node.identifier
         tree_dict[data].add_node(root_node)
-        print("name:", data, "id:", tree_dict[data].identifier)
         parent_id = root_node.identifier
         for bag in luggage_dict[data]:
-            print("data:", data, "bag:", bag[1])
-            print(tree_dict[data].identifier)
             tree_dict[data].create_node(tag = bag, data = data, parent = parent_id)
 
-    for data in luggage_dict:
-        tree_dict[data].show()
-    
+    # count every tree that contains a shiny gold bag
+    print("part #1:", sum([(search_luggage(luggage_dict, key, "shinygold")) for key in luggage_dict]) - 1)
+
+    # build tree of luggage containing shiny gold
+
     items = ['shinygold']
-
-    # search
-    while items:
-        print(items)
-        item = items.pop()
-        if item not in luggage_dict:
-            continue
-        for data in luggage_dict[item]:
-            print("'{}'".format(data[1]))
-            items.append(data[1])
-
     tree = Tree()
     node = Node('shinygold', data=int(1))
     items = [node]
@@ -56,7 +61,6 @@ def main():
 
     while items:
         parent_node = items.pop()
-        print(parent_node)
         parent_id = parent_node.identifier
         if parent_node.tag not in luggage_dict:
             continue
@@ -65,9 +69,7 @@ def main():
             tree.add_node(node, parent = parent_id)
             items.append(node)
 
-    tree.show()
-
-    # pre order traversal
+    # preorder traversal to distribute the luggage multiplier
     stack = [tree.get_node(tree.root)]
     output = []
 
@@ -76,13 +78,12 @@ def main():
         for child_node in tree.children(node.identifier):
             child_node.data *= node.data
         output.insert(0, node.data)
-        print(node.data, ",", end="")
 
         if tree.children(node.identifier):
             stack += reversed(tree.children(node.identifier))
 
-    print(output)
-    print(sum(output)-1)
+    # produce bag total
+    print("part #2:", sum(output)-1)
 
 if __name__ == "__main__":
     main()
