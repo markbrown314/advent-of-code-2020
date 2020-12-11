55cf1bf14eea299f4d763b84032e41b37af0f7b7
Mark F. Brown <mark.brown314@gmail.com>
2020-12-10 22:47:09 -0500

    Solution for Day 9 Part 1 and 2
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-9/solution.py
+++ day-9/solution.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
 """
-🎅 Advent of Code 2020 Day #9
+🎅 Advent of Code 2020 Day #9 Part 1 & 2
    by Mark F. Brown <mark.brown314@gmail.com>
 """
 import itertools
 import sys
 
 def main():
-    filename = "puzzle_test.txt" if len(sys.argv) < 2 else sys.argv[1]
+    filename = "puzzle_input.txt" if len(sys.argv) < 2 else sys.argv[1]
     file_input = [int(num) for num in open(filename).read().splitlines()]
-    print("file_input:", file_input)
 
     i = 0
-    n = 5
+    n = 25
     remainder = [*reversed(file_input[n:])]
-    while remainder:
-        
+
+# find number that is not the sum of a pair in previous 25 numbers
+    while remainder:       
         selection = file_input[i:n+i]
         pairs = [*itertools.combinations(selection, 2)]
-        print("selection:", selection)
-        print("remainder:", remainder)
         check = remainder.pop()
 
-        print("check:", check)
-        print("pairs:", pairs)
         additions = {a[0]+a[1]: (a[0], a[1]) for a in pairs}
-        print("additions:", additions)
 
         if check in additions:
-            print("found", check)
             i += 1
             continue
         else:
-            print("could not find", check)
+            print("part 1:", check)
             break
-# scan
-
+# scan for contiguous range that adds up to number found in part 1
+    mode = True
+    i = 0
+    n = 1
+    acc = file_input[0]
+    while i < n:
+        if acc == check:
+            print("part 2:", file_input[i]+file_input[n])
+            break
+        if mode:
+            acc += file_input[n]
+            if acc > check:
+                mode = False
+            n += 1
+        else:
+            acc -= file_input[i]
+            if acc < check:
+                mode = True
+            i += 1
 
 if __name__ == "__main__":
     main()
