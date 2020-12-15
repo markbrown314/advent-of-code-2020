e3585d602af84bb6bb0baac6a56c4d8f5d5288a1
Mark F. Brown <mark.brown314@gmail.com>
2020-12-14 19:55:01 -0500

    Day 10 solution with comments and pylint
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-10/solution.py
+++ day-10/solution.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
+"""
+🎅 Advent of Code 2020 Day #10 Adapter Array Part 1 & 2
+   by Mark F. Brown <mark.brown314@gmail.com>
+"""
 import sys
 
 def main():
-    filename = "puzzle_test_1.txt" if len(sys.argv) < 2 else sys.argv[1]
+    """ solution for Day 10 """
+    filename = "puzzle_input.txt" if len(sys.argv) < 2 else sys.argv[1]
     file_input = [int(num) for num in open(filename).read().splitlines()]
+    # find distribution of joltage differences 3's and 1's
     jolts = sorted(file_input)
-    jolts.insert(0,0)
+    # add first and last item
+    last_item = jolts[-1]
+    jolts.insert(0, 0)
+    jolts.append(last_item + 3)
+
     dist = []
     for i in range(0, len(jolts)-1):
         dist.append(jolts[i+1]-jolts[i])
-    
-    print(jolts)
-    print(dist)
-    threes = len([*filter(lambda a: a == 3, dist)]) + 1
+
+    threes = len([*filter(lambda a: a == 3, dist)])
     ones = len([*filter(lambda b: b == 1, dist)])
 
-    print("ones:", ones, "threes:", threes, ones * threes)
+    print("part 1: ones:", ones, "threes:", threes, "answer:", ones * threes)
 
-    paths = sorted(file_input, reverse = True)
-    first_path = paths[0]
-    paths.insert(0, first_path+3)
-    paths.append(0)
-    print(paths)
-    path_dict = {path:0 for path in paths}
-    path_dict[paths[0]] = 1
-    print(paths)
-    print(path_dict)
-    for i in paths:
+    # model connections as a tree (count roots inorder to determine total paths)
+    jolts = sorted(file_input, reverse = True)
+    # add first and last item
+    first_item = jolts[0]
+    jolts.insert(0, first_item + 3)
+    jolts.append(0)
+
+    # use a dictionary to count roots of tree
+    path_dict = {path:0 for path in jolts}
+    path_dict[jolts[0]] = 1
+
+    # use memoization technique to cache paths already counted
+    for i in jolts:
         total = path_dict[i]
         if i+1 in path_dict:
             total += path_dict[i+1]
@@ -36,8 +47,7 @@ def main():
             total += path_dict[i+3]
         path_dict[i] = total
 
-    print(path_dict)
-    print("final count:", path_dict[0])
+    print("part 2: distinct arrangement:", path_dict[0])
 
 if __name__ == "__main__":
     main()
